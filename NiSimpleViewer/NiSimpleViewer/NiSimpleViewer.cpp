a329a6c8c0f9ed78e66ee0e381bb4515f4dbee21
lips-hci <alenchen@lips-hci.com>
2017-07-12 13:09:52 +0800

    Add IR view in simple viewer

--- NiSimpleViewer/NiSimpleViewer/NiSimpleViewer.cpp
+++ NiSimpleViewer/NiSimpleViewer/NiSimpleViewer.cpp
@@ -18,12 +18,15 @@ int _tmain(int argc, _TCHAR* argv[])
     ImageGenerator mImageGen;
     mImageGen.Create( mContext );
 
+    IRGenerator mIrGen;
+    mIrGen.Create( mContext );
+
     mContext.StartGeneratingAll();
 
     char fpsstr[7];
     double tStart = 0;
 
-    while ( true )
+    while ( !xnOSWasKeyboardHit() )
     {
         tStart = (double)getTickCount();
         mContext.WaitAndUpdateAll();
@@ -32,7 +35,7 @@ int _tmain(int argc, _TCHAR* argv[])
         mDepthGen.GetMetaData( depthData );
         Mat imgDepth( depthData.FullYRes(), depthData.FullXRes(), CV_16UC1, ( void* )depthData.Data() );
         Mat img8bitDepth;
-        imgDepth.convertTo( img8bitDepth, CV_8U, 255.0 / 5000 );
+        imgDepth.convertTo( img8bitDepth, CV_8U, 255.0 / 4096 );
         sprintf_s(fpsstr, sizeof(fpsstr), "%.2f", 1.0 / (((double)getTickCount() - tStart) / getTickFrequency()));
         putText(img8bitDepth, string("FPS:") + fpsstr, Point(5, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(200, 0, 0));
         imshow( "Depth view", img8bitDepth );
@@ -46,8 +49,20 @@ int _tmain(int argc, _TCHAR* argv[])
         putText(imgBGRColor, string("FPS:") + fpsstr, Point(5, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(200, 0, 0));
         imshow( "Color view", imgBGRColor );
 
+        IRMetaData irData;
+        mIrGen.GetMetaData( irData );
+        Mat imgIR( irData.FullYRes(), irData.FullXRes(), CV_16UC1, ( void * )irData.Data() );
+        Mat img8bitIR;
+        imgIR.convertTo( img8bitIR, CV_8U, 255.0 / 4096 );
+        sprintf_s(fpsstr, sizeof(fpsstr), "%.2f", 1.0 / (((double)getTickCount() - tStart) / getTickFrequency()));
+        putText(img8bitIR, string("FPS:") + fpsstr, Point(5, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(200, 0, 0));
+        imshow( "IR view", img8bitIR );
+
         waitKey( 1 );
     }
+    mDepthGen.Release();
+    mImageGen.Release();
+    mContext.Release();
     return 0;
 }
 
