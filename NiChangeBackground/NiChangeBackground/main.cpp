5c00948492cf9eee416ed938d772a1b4cdd2ba60
Alucard Lu <AlucardLu@lips-hci.com>
2017-08-02 13:17:25 +0800

    Fix typo and release mode issue.
    	1. remove some global variable
    	2. Add release Usergenerator
    	3. Add UnregisterUserCallbacks before release usergenerator
    
    	modified:   NiChangeBackground/NiChangeBackground/NiChangeBackground.vcxproj
    	modified:   NiChangeBackground/NiChangeBackground/main.cpp

--- NiChangeBackground/NiChangeBackground/main.cpp
+++ NiChangeBackground/NiChangeBackground/main.cpp
@@ -11,39 +11,7 @@ using namespace std;
 using namespace xn;
 using namespace cv;
 
-#define DEBUG 1
-cv::Mat colorBG;
-xn::Device mDevice;
-vector<std::string> filenames;
-int files = 0;
-
-int handleEvent ( char keyEvent )
-{
-    int value = 0;
-    if ( keyEvent == -1 || keyEvent == 255 ) return value;
-#ifdef DEBUG
-    cout<<"Receive key event: "<<keyEvent<<endl;
-#endif
-    switch (keyEvent)
-    {
-        case 27:            //esc key
-            value = -1;
-            break;
-        case ' ':
-            if (files == ( filenames.size() - 1) )
-            {
-                files = 0;
-            }
-            else
-            {
-                files++;
-            }
-            break;
-        default:
-            break;
-    }
-    return value;
-}
+#define DEBUG 0
 
 // Callback: New user was detected
 void XN_CALLBACK_TYPE User_NewUser(xn::UserGenerator& /*generator*/, XnUserID nId, void* /*pCookie*/)
@@ -85,12 +53,14 @@ int main( int argc, char* argv[] )
     mUserGen.RegisterUserCallbacks(User_NewUser, User_LostUser, NULL, hUserCallbacks);
     mDepthGen.GetAlternativeViewPointCap().SetViewPoint( mImageGen );
     mContext.StartGeneratingAll();
+    vector<std::string> filenames;
+    int files = 0;
     filenames.push_back ("bg\\1.jpg");
     filenames.push_back ("bg\\2.jpg");
 
     while (true)
     {
-        colorBG = cv::imread( filenames[files].c_str() );
+        Mat colorBG = cv::imread( filenames[files].c_str() );
         cv::resize( colorBG, colorBG, Size(640, 480 ));
         mContext.WaitOneUpdateAll( mUserGen );
         xn::DepthMetaData depthData;
@@ -120,8 +90,23 @@ int main( int argc, char* argv[] )
         }
         imshow( "Depth view", img8bitDepth );
         imshow( "Color view", colorBG );
-        if ( handleEvent( waitKey( 1 ) )  == -1 ) break;
+        // handle key event
+        int keyEvent = waitKey ( 1 );
+#if DEBUG
+         cout<<"receive key event: "<<keyEvent<<endl;
+#endif
+        if ( 27 == keyEvent ) break;
+        if ( 32 == keyEvent )
+        {
+            files++;
+            if( files == filenames.size() )
+            {
+                files = 0;
+            }
+        }
     }
+    mUserGen.UnregisterUserCallbacks(hUserCallbacks);
+    mUserGen.Release();
     mImageGen.Release();
     mDepthGen.Release();
     mContext.Release();
